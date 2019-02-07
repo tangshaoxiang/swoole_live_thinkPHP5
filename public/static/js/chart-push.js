31e9a882efe4a2c21aec820fd5e41ba981f90592
darian <873246925@qq.com>
2019-02-07 23:52:46 +0800

    't_up'

--- public/static/js/chart-push.js
+++ public/static/js/chart-push.js
@@ -2,7 +2,7 @@ $(function () {
     $('#discuss-box').keydown(function (event) {
         if(event.keyCode == 13){
             var text = $(this).val();
-            var url  = "http://www.darian.shop:8811/?s=index/chart/index"
+            var url  = "http://www.darian.xin:8811/?s=index/chart/index"
             var data = {'content':text,'game_id':1};
             $.post(url,data,function (data) {
                 //todo
