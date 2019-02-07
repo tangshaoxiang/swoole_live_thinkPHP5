fe6af52ff514ae7b621ad2cc4ad5ba1c8e81f452
darian <873246925@qq.com>
2019-02-08 00:56:43 +0800

    't_up'

---
+++ application/common/lib/redis/Tredis.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ty_Ro
+ * Date: 2019/2/8
+ * Time: 0:54
+ */
+namespace app\common\lib\redis;
+class Tredis {
+    public $redis = "";
+    /**
+     * 定义单例模式的变量
+     * @var null
+     */
+    private static $_instance = null;
+
+    public static function getInstance() {
+        if(empty(self::$_instance)) {
+            self::$_instance = new self();
+        }
+        return self::$_instance;
+    }
+
+    private function __construct() {
+    }
+
+    public function test($key){
+        return 11111111111;
+    }
+}
