c6ab5ba6bb657e52fa79c365e8e7781215d0d8ce
darian <873246925@qq.com>
2019-02-08 00:32:27 +0800

    't_up'

--- server/websocket.php
+++ server/websocket.php
@@ -39,7 +39,7 @@ class Http {
      */
     public function onOpen($http, $request) {
         // fd redis [1]
-        \app\common\lib\redis\Predis::getInstance()->sAdd(config('redis.live_game_key'), $request->fd);
+        \application\common\lib\redis\Predis::getInstance()->sAdd(config('redis.live_game_key'), $request->fd);
         var_dump($request->fd);
     }
 
